0ad6c99ed7f07238f7e43cd56b921c323d9a32e3
Ben <49142757+MBreverse@users.noreply.github.com>
2024-06-19 17:13:17 +0800

    update

--- Cifar100_10by10_task/NoSample/Knowledge_Distillation/FixKD_Only@10-10-Cifar100/ModelCollection.py
+++ Cifar100_10by10_task/NoSample/Knowledge_Distillation/FixKD_Only@10-10-Cifar100/ModelCollection.py
@@ -4,14 +4,20 @@ import torch.nn as nn
 import torch
 import torch.nn.parameter as parameter
 
-
     
-def init_weights_with_zeros(m):
-    m.weight.data.fill_(0.)
-
-def init_weights_with_ones(m):
-    m.weight.data.fill_(1.)
-    
+class FeatureExtractor(nn.Module):
+   
+    def __init__(self, mode):
+        super().__init__()
+        
+        if mode == "res50":
+            self.model = Resnet50_FeatureExtrator()
+        
+        if mode == "vgg16":
+            self.model = None # update after
+    def forward(self, x):
+        return sself.model(x)
+       
 class Resnet50_FeatureExtrator(nn.Module):
 
     def __init__(self):
@@ -41,8 +47,7 @@ class CNN(nn.Module):
         self.fc1 = nn.Linear(2048,1024)
         # self.fc2 = nn.Linear(1024,1024)
         self.fc2 = nn.Linear(1024,classes)
-        
-        
+                
     def forward(self, x, mode = 'img'):
         
         if  mode == "img":
@@ -55,238 +60,40 @@ class CNN(nn.Module):
             feature = x.view(-1,2048)
             
         resp = self.fc1(feature)
-        resp = nn.functional.dropout(resp, 0.25, training=True, inplace=False)
         resp = nn.functional.relu(resp, inplace=False) 
-        
+        resp = nn.functional.dropout(resp, p=0.25, training = self.training, inplace=False)              
         output = self.fc2(resp)
         # resp = nn.functional.relu(resp, inplace=False)         
         # output = self.fc3(resp)
         
         return output
 
-class ScaleKernel(nn.Module):
-  def __init__(self, size_of_kernel):
-    super().__init__()
+class AutoEncoderWithCLF(nn.Module):
 
-    tensor = torch.rand(size_of_kernel)
-    self.weight = parameter.Parameter(tensor, requires_grad=True)      
-
-  def forward(self, x):
-
-    y = x * self.weight 
-
-    return y
-    
-class ShiftKernel(nn.Module):
-  def __init__(self, size_of_kernel):
-    super().__init__()
-
-    tensor = torch.rand(size_of_kernel)
-    self.weight = parameter.Parameter(tensor, requires_grad=True)      
-
-  def forward(self, x):
-
-    y = x + self.weight 
-
-    return y
-        
-class Single_Scale_Shift_Kernel(nn.Module):
-  def __init__(self, size_of_kernel, training = True):
-    super().__init__()
-
-    self.scale = ScaleKernel(size_of_kernel)
-    self.shift = ShiftKernel(size_of_kernel) 
-    
-    self.scale.apply(init_weights_with_ones)
-    self.shift.apply(init_weights_with_zeros)  
-    
-    for name, value in self.scale.named_parameters():
-    
-                if training == True:
-                    value.requires_grad = True 
-                else:
-                    value.requires_grad = False    
-    
-    for name, value in self.shift.named_parameters():
-    
-                if training == True:
-                    value.requires_grad = True 
-                else:
-                    value.requires_grad = False     
- 
-  def forward(self, x):
-
-    y = self.scale(x)
-    y = self.shift(y)
-
-    return y
-    
-class Multi_Scale_Shift_Kernel(nn.Module):
-  def __init__(self, insertion_check_list, sizes_of_kernels, training = True):
-    super().__init__()
-             
-    self.kernel_list = nn.ModuleList([])  
-    
-    # print(insertion_check_list)
-    sizes_of_kernels = iter(sizes_of_kernels)
-    for insert_check in insertion_check_list:
-    
-        if insert_check == True:          
-            size_of_kernel = next(sizes_of_kernels)
-            # print(size_of_kernel)
-            self.kernel_list.append( Single_Scale_Shift_Kernel(size_of_kernel))
-            
-        else:      
-            
-            self.kernel_list.append(None)
-    # print(self.kernel_list)
-             
-
-  # indicate which kernel to be used
-  def forward(self, x, index_of_kernel):
-  
-    if index_of_kernel <0:        
-        raise "error: no kernel mapping of input index!!!"
-        
-    else:    
-        selected_kernel = self.kernel_list[index_of_kernel]
-        if selected_kernel!= None:
-            #return shift and scale process output for kernel insertion here
-            y = selected_kernel(x)
-            
-        else:
-            #return input for no kernel insertion here
-            y = x
-
-
-    return y
-     
-class SS_Model(nn.Module):
-    def __init__(self, basic_model, insertion_check_list, sizes_of_kernel_set, all_kernel_list = []):
-        super().__init__()
-        
-        self.basic_model = basic_model
-        self.num_fc_layer = len(list(self.basic_model.children())[1:])
-        self.kernel_set1 = Multi_Scale_Shift_Kernel(insertion_check_list, sizes_of_kernel_set)  
-        self.kernel_set2 = Multi_Scale_Shift_Kernel(insertion_check_list, sizes_of_kernel_set)
-        self.all_kernel_set = nn.ModuleList(all_kernel_list)
-
-    def forward(self, x, inp_mode = 'img'):
-        
-        if  inp_mode == "img":
-        
-            feature = self.basic_model.FE(x)
-            feature = feature.detach().view(-1,2048) 
-            
-        elif inp_mode == "f":
-            
-            feature = x.view(-1,2048)
-            
-        output_list=[]
-        # cls1 forward
-        if self.kernel_set1 == None:
-                                                 
-            output1 = self.basic_model(feature, 'f')
-
-        else:
-            resp = feature 
-            for i, module in enumerate(list(self.basic_model.children())[1:]):
-                
-                if i != (self.num_fc_layer-1):
-                    #non-last layer forward
-                    resp = self.kernel_set1(resp, 2*i)
-                    resp = module(resp)
-                    resp = self.kernel_set1(resp, 1 + 2*i)               
-                    resp = nn.functional.relu(resp, inplace=False)
-                else:
-                    #last layer forward
-                    resp = self.kernel_set1(resp, 2*i) 
-                    output1 = module(resp)
-
-         
-        output_list.append(output1)
-        
-        if self.training ==True:
-          
-            resp = feature 
-            for i, module in enumerate(list(self.basic_model.children())[1:]):
-                
-                if i != (self.num_fc_layer-1):
-                    #non-last layer forward
-                    resp = self.kernel_set2(resp, 2*i)
-                    resp = module(resp)
-                    resp = self.kernel_set2(resp, 1 + 2*i)               
-                    resp = nn.functional.dropout(resp, 0.25, inplace=False)
-                    resp = nn.functional.relu(resp, inplace=False)
-                else:
-                    #last layer forward
-                    resp = self.kernel_set2(resp, 2*i) 
-                    output2 = module(resp)
-                    
-            output_list.append(output2)
-        else:
-        
-            # cls2 ~task N forward 
-            for j in range(0,len(self.all_kernel_set)):
-                                           
-                ss_kernels = self.all_kernel_set[j]
-                resp = feature 
-     
-                for i, module in enumerate(list(self.basic_model.children())[1:]):
-                    
-                    if i != (self.num_fc_layer-1):
-                        #non-last layer forward
-                        resp = ss_kernels(resp, 2*i)
-                        resp = module(resp)
-                        resp = ss_kernels(resp, 1 + 2*i)               
-                        resp = nn.functional.relu(resp, inplace=False)
-                    else:
-                        #last layer forward
-                        resp = ss_kernels(resp, 2*i) 
-                        output = module(resp)
-   
-                output_list.append(output)
-            
- 
-        return output_list
-
-class AutoEncoder(nn.Module):
-
-    def __init__(self):
+    def __init__(self, base_class = 50):
         super().__init__()
             
         self.en1 = nn.Linear(2048,1024)
         self.en2 = nn.Linear(1024,512)
-        self.en3 = nn.Linear(512,100)
+        self.en3 = nn.Linear(512,base_class)
                 
-        self.de1 = nn.Linear(100,512)
+        self.de1 = nn.Linear(base_class,512)
         self.de2 = nn.Linear(512,1024)
         self.de3 = nn.Linear(1024,2048)
         
-        self.EncoderModules = nn.Sequential(self.en1, self.en2, self.en3)
-        self.DecoderModules = nn.Sequential(self.de1, self.de2, self.de3)
+        # self.EncoderModules = nn.Sequential(self.en1, self.en2, self.en3)
+        # self.DecoderModules = nn.Sequential(self.de1, self.de2, self.de3)
+        self.clf = nn.Linear(100, 50)
 
     def forward(self, x):
+
+        z = self.encode(x)
+        x_head = self.decode(z)                   
+        cls_pred = self.clf(z)
         
-        encode1 = self.en1(x)
-        encode1 = torch.nn.functional.elu(encode1, alpha=1.0, inplace=False)
-        
-        encode2 = self.en2(encode1)    
-        encode2 = torch.nn.functional.elu(encode2, alpha=1.0, inplace=False)
-        
-        encode3 = self.en3(encode2)  # last layer
-        
-        decode1 = self.de1(x)
-        decode1 = torch.nn.functional.elu(decode1, alpha=1.0, inplace=False)
-        
-        decode2 = self.de2(decode1) 
-        decode2 = torch.nn.functional.elu(decode2, alpha=1.0, inplace=False)
-        
-        decode3 = self.de3(decode2) # last layer
-        
-        return encode3, decode3
+        return z, x_head, cls_pred
     
-    def loss(self, pred, true, mode = 'mse'):
+    def loss(self, pred, true, origin_classes = 0, mode = 'mse'):
     
         if mode == "mse":
             mse_loss = torch.nn.MSELoss(size_average=None, reduce=None, reduction='mean')
@@ -295,22 +102,193 @@ class AutoEncoder(nn.Module):
         elif mode == "cross_entropy":
             en_loss = torch.nn.CrossEntropyLoss(weight=None, reduction='mean')
             loss = en_loss(pred, true)
+            
+        elif mode == "bce":
+            # bce_loss = torch.nn.BCELoss()
+            bce_loss = torch.nn.BCEWithLogitsLoss()            
+            loss = bce_loss(pred, true.float())
+            
+        elif mode == "bce_kd":
+            bce_loss = torch.nn.BCEWithLogitsLoss()
+            sigmoid_module = nn.Sigmoid()
+            true = sigmoid_module(true)
+            loss = bce_loss(pred[:,0:origin_classes], true)
                         
         return loss
     
     def encode(self, x):
         '''Enocder used oly'''
         encode1 = self.en1(x)
+        encode1 = nn.functional.dropout(encode1, p=0.25, training = self.training, inplace=False)  
         encode1 = torch.nn.functional.elu(encode1, alpha=1.0, inplace=False)
         
-        encode2 = self.en2(encode1)    
+        encode2 = self.en2(encode1)   
+        encode2 = nn.functional.dropout(encode2, p=0.25, training = self.training, inplace=False)          
         encode2 = torch.nn.functional.elu(encode2, alpha=1.0, inplace=False)
         
         encode3 = self.en3(encode2)  # last layer    
-        
+
         return encode3
 
     def decode(self, x):
+        '''Decoder used oly'''  
+        # x = torch.nn.functional.sigmoid(x)
+        # x = torch.nn.functional.elu(x, alpha=1.0, inplace=False)
+        decode1 = self.de1(x)
+        # decode1 = nn.functional.dropout(decode1, p=0.25, training = self.training, inplace=False)  
+        decode1 = torch.nn.functional.elu(decode1, alpha=1.0, inplace=False)
+        
+        decode2 = self.de2(decode1) 
+        # decode2 = nn.functional.dropout(decode2, p=0.25, training = self.training, inplace=False)  
+        decode2 = torch.nn.functional.elu(decode2, alpha=1.0, inplace=False)
+        
+        decode3 = self.de3(decode2) # last layer    
+
+        return decode3   
+    
+    def classify(self, z):
+    
+        z = torch.nn.functional.elu(z, alpha=1.0, inplace=False)
+        cls_pred = self.clf(z)
+        
+        
+        return cls_pred
+
+class AutoEncoderwithFE(nn.Module):
+    
+    def __init__(self, base_class = 50, feature_size = 2048):
+        super().__init__()
+        
+        self.FE = Resnet50_FeatureExtrator()
+        self.AE = AutoEncoder(base_class)
+        self.feature_size = feature_size
+        
+    def forward(self, x, inp_mode = 'img'):
+        
+        if inp_mode == 'img':
+            
+            feature = self.FE(x)
+            feature = feature.detach().view(-1,self.feature_size )
+        
+        elif inp_mode == 'f':
+            feature = x.view(-1,self.feature_size )
+        
+        
+        encode, decode = self.AE(feature)
+        
+        return encode, decode
+        
+    def encode(self, x,  inp_mode = 'img'):
+    
+        if inp_mode == 'img':
+            
+            feature = self.FE(x)
+            feature = feature.detach().view(-1,self.feature_size )
+        
+        elif inp_mode == 'f':
+            feature = x.view(-1,self.feature_size )    
+        
+        zcode = self.AE.encode(feature)
+        
+        return zcode
+        
+    def decode(self, x):   
+        
+        return self.AE.decode(x)      
+
+class Encoder(nn.Module):
+    def __init__(self, base_class):
+        super().__init__()  
+        
+        self.en1 = nn.Linear(2048,1024)
+        self.en2 = nn.Linear(1024,512)
+        self.en3 = nn.Linear(512,base_class)
+    
+    def forward(self, x):
+        encode1 = self.en1(x)
+        encode1 = nn.functional.dropout(encode1, p=0.25, training = self.training, inplace=False)  
+        encode1 = torch.nn.functional.elu(encode1, alpha=1.0, inplace=False)
+        
+        encode2 = self.en2(encode1)   
+        encode2 = nn.functional.dropout(encode2, p=0.25, training = self.training, inplace=False)          
+        encode2 = torch.nn.functional.elu(encode2, alpha=1.0, inplace=False)        
+        encode3 = self.en3(encode2)
+        
+        return encode3
+
+class Decoder(nn.Module):
+    def __init__(self, base_class):
+        super().__init__()  
+        
+        self.de1 = nn.Linear(base_class,512)
+        self.de2 = nn.Linear(512,1024)
+        self.de3 = nn.Linear(1024,2048)
+    
+    def forward(self, x):
+        '''Decoder used oly'''  
+
+        decode1 = self.de1(x)
+        decode1 = torch.nn.functional.elu(decode1, alpha=1.0, inplace=False)
+        
+        decode2 = self.de2(decode1) 
+        decode2 = torch.nn.functional.elu(decode2, alpha=1.0, inplace=False)
+        
+        decode3 = self.de3(decode2) # last layer    
+        
+        return decode3    
+
+class AutoEncoder(nn.Module):
+
+    def __init__(self, base_class = 50):
+        super().__init__()
+                
+        self.Encoder = Encoder(base_class)
+        self.Decoder = Decoder(base_class)
+        
+    def forward(self, x):
+
+        z = self.encode(x)
+        x_head = self.decode(z)                   
+        
+        return z, x_head
+
+    def encode(self, x): 
+        
+        z = self.Encoder(x)   
+
+        return z
+
+    def decode(self, x):
+        
+        x_h = self.Decoder(x)
+
+        return x_h   
+        
+    def loss(self, pred, true, origin_classes = 0, mode = 'mse'):
+    
+        if mode == "mse":
+            mse_loss = torch.nn.MSELoss(size_average=None, reduce=None, reduction='mean')
+            loss = mse_loss(pred, true)
+        
+        elif mode == "cross_entropy":
+            en_loss = torch.nn.CrossEntropyLoss(weight=None, reduction='mean')
+            loss = en_loss(pred, true)
+            
+        elif mode == "bce":
+            # bce_loss = torch.nn.BCELoss()
+            bce_loss = torch.nn.BCEWithLogitsLoss()            
+            loss = bce_loss(pred, true.float())
+            
+        elif mode == "bce_kd":
+            bce_loss = torch.nn.BCEWithLogitsLoss()
+            sigmoid_module = nn.Sigmoid()
+            true = sigmoid_module(true)
+    
+            loss = bce_loss(pred[:,0:origin_classes], true[:,0:origin_classes])
+                        
+        return loss
+    
+
         '''Decoder used oly'''    
         decode1 = self.de1(x)
         decode1 = torch.nn.functional.elu(decode1, alpha=1.0, inplace=False)
@@ -321,3 +299,93 @@ class AutoEncoder(nn.Module):
         decode3 = self.de3(decode2) # last layer    
 
         return decode3                       
+class CVAE(nn.Module):
+    def __init__(self, feature_size, latent_size, class_size):
+        super(CVAE, self).__init__()
+        
+        self.feature_size = feature_size
+        self.class_size = class_size
+
+        # encode        
+        self.encoder = CVAEEncoder(latent_size, feature_size, class_size)
+
+        # decode
+        self.decoder = CVAEDecoder(latent_size, feature_size, class_size)
+
+        self.elu = nn.ELU()
+        self.sigmoid = nn.Sigmoid()
+
+    def encode(self, x, c): # Q(z|x, c)
+        '''
+        x: (bs, feature_size)
+        c: (bs, class_size)
+        '''
+        z_mu, z_var = self.encoder(x, c)
+        
+        return z_mu, z_var
+
+    def reparameterize(self, mu, logvar):
+        std = torch.exp(0.5*logvar)
+        eps = torch.randn_like(std)
+        return mu + eps*std
+
+    def decode(self, z, c): # P(x|z, c)
+        '''
+        z: (bs, latent_size)
+        c: (bs, class_size)
+        '''
+        xhead = self.decoder(z, c)
+        
+        # no sigmoid because of Cifar100
+        
+        return xhead#self.sigmoid()
+
+    def forward(self, x, c):
+        mu, logvar = self.encode(x.view(-1, self.feature_size), c)
+        z = self.reparameterize(mu, logvar)
+        return self.decode(z, c), mu, logvar        
+
+class CVAEEncoder(nn.Module):
+    def __init__(self, latent_size, feature_size, class_size):
+        super().__init__()  
+        
+        hidden_dim = 400
+        self.ShareFC  = nn.Linear(feature_size + class_size, hidden_dim)
+        self.MeanEncoder = nn.Linear(hidden_dim, latent_size)
+        self.VarEncoder = nn.Linear(hidden_dim, latent_size)
+
+    
+    def forward(self, x, c):
+        '''Decoder used oly'''  
+        
+        inputs = torch.cat([x, c], 1) # (bs, feature_size+class_size)        
+        h1 = self.ShareFC(inputs)
+        h1 = torch.nn.functional.elu(h1, alpha=1.0, inplace=False) 
+        z_mu = self.MeanEncoder(h1)
+        z_var = self.VarEncoder(h1) 
+        
+        return  z_mu, z_var
+
+class CVAEDecoder(nn.Module):
+    def __init__(self, laten_size, feature_size, class_size):
+        super().__init__()  
+        
+        hidden_dim = 400
+        self.de1 = nn.Linear(laten_size + class_size, hidden_dim)
+        self.de2 = nn.Linear(hidden_dim,feature_size)
+    
+    def forward(self, z, c):
+        '''Decoder used oly'''  
+        
+        inputs = torch.cat([z, c], 1) # (bs, latent_size+class_size)
+        
+        decode1 = self.de1(inputs)
+        decode1 = torch.nn.functional.elu(decode1, alpha=1.0, inplace=False)        
+        decode2 = self.de2(decode1) # last layer    
+        
+        return decode2    
+
+
+if __name__ == "__main__" :
+   
+   print(__name__)
